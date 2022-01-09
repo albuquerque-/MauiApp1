e4a1d9aca8818a6393a8ee9e42b401cfc54ea9c4
Pedro <Pedro@PEDRO2>
2022-01-09 17:26:32 +0000

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    interface IImcCalc
+    {
+        int getValue10(int i);
+    }
+    public class Class1:IImcCalc
+    {
+        public int getValue10(int i)
+        {
+            return i * 10;
+        }
+    }
+}
---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibrary1;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        Class1 class1 = new Class1();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Current count: {count}";
+
+            SemanticScreenReader.Announce(CounterLabel.Text);
+            
+            int result = class1.getValue10(count);
+
+            CounterLabelCalc.Text = $"Using IImcCalc on a dll library: {result}";
+
+        }
+    }
+}
